e86bff324a12293f736db111f913e8d2b124ec9d
vvyugovskiy <58443033+vvyugovskiy@users.noreply.github.com>
2020-04-21 15:15:14 -0400

    Update GitPractice.java

--- src/test/java/GitPractice.java
+++ src/test/java/GitPractice.java
@@ -5,6 +5,8 @@ public class GitPractice {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
+        
+        System.out.println("I love Java");
 
     }
 }
