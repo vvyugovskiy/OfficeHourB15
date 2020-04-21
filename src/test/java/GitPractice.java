73a943bbb31271d745ca83c54a98262257c4b163
vvyugovskiy <resifar@gmail.com>
2020-04-21 15:32:27 -0400

    the other

--- src/test/java/GitPractice.java
+++ src/test/java/GitPractice.java
@@ -6,5 +6,8 @@ public class GitPractice {
         System.out.println("Hello World");
         System.out.println("Hello World");
 
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
     }
 }
